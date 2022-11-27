d74868ce6d5e46cb4e2cb4710775ae8d6c4919cd
Igor Nascimento <igor.nascimento@objectedge.com>
2022-11-27 18:54:23 -0300

    Project initialization

---
+++ groomer/src/main/java/com/invoce/groomer/InvoiceGroomerApplication.java
@@ -0,0 +1,13 @@
+package com.invoce.groomer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InvoiceGroomerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InvoiceGroomerApplication.class, args);
+	}
+
+}
---
+++ groomer/src/test/java/com/invoce/groomer/InvoiceGroomerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.invoce.groomer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InvoiceGroomerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
